751f97eaeb1fb25e2877f6992acb31c31c6ea202
nhantran3395 <thanhnhan33.95@gmail.com>
2026-01-31 22:16:14 +0700

    Setup Spectrum V2 and use headless CopilotKit

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,20 +1,77 @@
-import { CopilotKitProvider, CopilotSidebar } from "@copilotkit/react-core/v2";
-import "@copilotkit/react-core/v2/styles.css";
+import {
+  CopilotKitProvider,
+  useAgent,
+  useCopilotKit,
+} from "@copilotkit/react-core/v2";
+import { Provider, Button, TextArea } from "@react-spectrum/s2";
+import type { Message } from "@copilotkit/shared";
+import { useState } from "react";
+
+function renderChatMessage(message: Message): string {
+  switch (message.role) {
+    case "user":
+    case "assistant":
+    case "system": {
+      if (typeof message.content === "string") {
+        return message.content;
+      }
+      return "";
+    }
+
+    default: {
+      return "";
+    }
+  }
+}
 
 function App() {
   return (
     <CopilotKitProvider runtimeUrl="http://localhost:4000/copilotkit">
-      <MainContent />
-      <CopilotSidebar agentId="personal_assistant_agent" />
+      <Provider background="base">
+        <MainContent />
+      </Provider>
     </CopilotKitProvider>
   );
 }
 
 function MainContent() {
+  const { copilotkit } = useCopilotKit();
+  const [userInput, setUserInput] = useState<string>("");
+
+  const { agent } = useAgent({
+    agentId: "personal_assistant_agent",
+  });
+
+  const onSendMessage = () => {
+    agent.addMessage({
+      id: crypto.randomUUID(),
+      role: "user",
+      content: userInput,
+    });
+    copilotkit.runAgent({ agent });
+  };
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>My App with CopilotKit v2 + Google ADK</h1>
-      <p>Open the sidebar to chat with the AI agent.</p>
+    <div>
+      <section></section>
+      <section>
+        {agent.messages.map((message) => (
+          <div key={message.id} style={{ marginBottom: "0.5rem" }}>
+            {renderChatMessage(message)}
+          </div>
+        ))}
+      </section>
+      <div>
+        <TextArea
+          placeholder="Message ..."
+          inputMode="text"
+          value={userInput}
+          onChange={(event) => setUserInput(event)}
+        />
+        <Button variant="genai" onPress={onSendMessage}>
+          Send
+        </Button>
+      </div>
     </div>
   );
 }
